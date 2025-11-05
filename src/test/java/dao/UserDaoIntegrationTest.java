b76f1363b9e1e2d7e15a79f32bb6733ac3fa58b3
Christopher Sweet <christopher.sweet@wsu.edu>
2025-11-05 14:37:33 -0800

    Initial assignment setup. Dependencies added.

---
+++ src/test/java/dao/UserDaoIntegrationTest.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoIntegrationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
